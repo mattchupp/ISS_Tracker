c8d82e73f19248004cd1fcb26aeda483a3228739
mattchupp <mattlchupp@gmail.com>
2020-06-02 08:01:53 -0500

    get location and set const for api call

--- src/PassTimes.js
+++ src/PassTimes.js
@@ -1,24 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import axios from 'axios'; 
 
 function PassTimes() {
 
   // set state
-  const [lat, setLat] = useState('40.116421');
-  const [long, setLong] = useState('88.243385');
+  // const [lat, setLat] = useState('40.116421');
+  // const [long, setLong] = useState('88.243385');
+  const [lat, setLat] = useState('');
+  const [long, setLong] = useState('');
+  const [gotLocation, setGotLocation] = useState(false);
+  // const [passTimes, setPassTimes] = useState([]);
+  // const [test, setTest] = useState(); 
+
 
   useEffect(() => {
-    axios.get('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=40.116421&lon=-88.243385')
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      setGotLocation(true); 
+      
+    }); 
+  }, []);
+
+  useEffect(() => {
+    
+
+    console.log(lat); 
+      console.log(long); 
+    // console.long(gotLocation)
+    const api = `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${long}`
+    console.log(api);
+    /*
+    // if(gotLocation === true) {
+      axios.get(`https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${long}`)
       .then(response => {
+        
         console.log(response.data)
+        // setPassTimes(response.data); 
+        
+        // console.log(passTimes)
       })
       .catch(error => {
         console.log(error)
       });
-  });
+    // }
+      */
+    // console.log(gotLocation)
+    
+  }, [lat,long]);
+
+  // // console.log(test)
+  // setTest('Hello')
+  // console.log(test)
 
   return (
-    <div className="App">
+    <div>
       
     </div>
   );
