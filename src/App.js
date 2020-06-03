4958e76a0ae2393d86c7426133ee2c16bb224d2e
mattchupp <mattlchupp@gmail.com>
2020-06-02 22:47:28 -0500

    align all content in the center

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
 import PassTimes from './PassTimes';
+import styled from 'styled-components'; 
+
+const Container = styled.div`
+  display: flex; 
+  height: 100vh; 
+  width: 100%; 
+  justify-content: center; 
+  align-items: center; 
+`;
 
 function App() {
   return (
-    <div>
+    <Container>
       <PassTimes />
-    </div>
+    </Container>
   );
 }
 
