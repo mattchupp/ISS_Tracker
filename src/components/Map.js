3d2ce2c3a81f431b0b0fd7f740f3996335ece314
mattchupp <mattlchupp@gmail.com>
2020-06-03 22:45:56 -0500

    map working, sort of

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Circle } from 'google-maps-react';
 // require('dotenv').config({ path: '../../.env' })
 
 const mapStyles = {
   width: '500px',
-  height: '500px'
+  height: '500px',
+  position: 'relative'
 };
 
 export class MapContainer extends Component {
@@ -14,11 +15,26 @@ export class MapContainer extends Component {
         google={this.props.google}
         zoom={3}
         style={mapStyles}
-        initialCenter={{
+        center={{
          lat: this.props.lat,
          lng: this.props.long
         }}
+      >
+        <Circle
+        radius={1200}
+        center={{
+          lat: this.props.lat,
+          lng: this.props.long
+         }}
+        strokeColor='transparent'
+        strokeOpacity={0}
+        strokeWeight={5}
+        fillColor='#FF0000'
+        fillOpacity={0.2}
       />
+
+      </Map>
+      
     );
   }
 }
